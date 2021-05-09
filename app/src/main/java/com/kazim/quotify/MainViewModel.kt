084fede9dc86ddf11c1b3e985b1923cd4041f967
kazimsayed954 <kazimsayed954@gmail.com>
2021-05-09 16:56:01 +0530

    initial commit

---
+++ app/src/main/java/com/kazim/quotify/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.kazim.quotify
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import java.nio.charset.Charset
+
+class MainViewModel(val context: Context):ViewModel() {
+    private var quoteList:Array<Quote> = emptyArray()
+    private var index:Int = 0
+    init {
+        quoteList = loadQuoteFromAssets()
+    }
+
+    private fun loadQuoteFromAssets(): Array<Quote> {
+        val inputStream = context.assets.open("quotes.json")
+        val size:Int = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+
+        val json = String(buffer, Charsets.UTF_8)
+        val gson = Gson()
+        return gson.fromJson(json, Array<Quote>::class.java)
+    }
+
+    fun getQuote() = quoteList[index]
+    fun nextQuote() = quoteList[++index]
+    fun prevQuote() = quoteList[--index]
+}
